0e705d9f70670b12e9f32e90bfcce1ad6e4a3ad2
elvaraelvara <107627367+elvaraelvara@users.noreply.github.com>
2023-07-16 09:06:11 +0700

    Update TransactionHistory.vue

--- src/components/TransactionHistory.vue
+++ src/components/TransactionHistory.vue
@@ -1,44 +1,45 @@
 <template>
-    <div>
-      <h2>Transaction History</h2>
-      <ul>
-        <li v-for="transaction in transactions" :key="transaction.id">
-          <p>Movie: {{ transaction.movie }}</p>
-          <p>Seat Number: {{ transaction.seatNumber }}</p>
-          <p>Total Cost: {{ transaction.totalCost }}</p>
-          <button @click="cancelTransaction(transaction.id)">Cancel</button>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'TransactionHistory',
-    data() {
-      return {
-        transactions: []
-      }
+  <div>
+    <h2>Transaction History</h2>
+    <ul>
+      <li v-for="transaction in transactions" :key="transaction.id">
+        <p>Movie: {{ transaction.movie }}</p>
+        <p>Seat Number: {{ transaction.seatNumber }}</p>
+        <p>Total Cost: {{ transaction.totalCost }}</p>
+        <button @click="cancelTransaction(transaction.id)">Cancel</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'TransactionHistory',
+  data() {
+    return {
+      transactions: []
+    }
+  },
+  created() {
+    this.fetchTransactions()
+  },
+  methods: {
+    fetchTransactions() {
+      axios.get('https://seleksi-sea-2023.vercel.app/api/transactions')
+        .then(response => {
+          this.transactions = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
-    created() {
-      this.fetchTransactions()
-    },
-    methods: {
-      fetchTransactions() {
-        // Fetch transaction history from API for the current user
-        // Example using Axios library
-        axios.get('https://seleksi-sea-2023.vercel.app/api/transactions')
-          .then(response => {
-            this.transactions = response.data
-          })
-          .catch(error => {
-            console.log(error)
-          })
-      },
-      cancelTransaction(transactionId) {
-        // Logic to cancel the transaction and update transactions list
-      }
+    cancelTransaction(transactionId) {
+
+      const updatedTransactions = this.transactions.filter(transaction => transaction.id !== transactionId);
+      this.transactions = updatedTransactions;
     }
   }
-  </script>
-  
+}
+</script>
