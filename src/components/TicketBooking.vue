d287d6d3b65636667a1a743e3ecac967f245b4cc
elvaraelvara <107627367+elvaraelvara@users.noreply.github.com>
2023-07-16 09:04:55 +0700

    Update TicketBooking.vue

--- src/components/TicketBooking.vue
+++ src/components/TicketBooking.vue
@@ -6,7 +6,75 @@
       <form @submit.prevent="bookTicket">
         <label for="seatNumber">Seat Number:</label>
         <input type="number" id="seatNumber" v-model="seatNumber" min="1" max="64" required>
-        <label for="name">Name:</label>
+        <label for="name">Name:</label><template>
+  <div>
+    <h2>Ticket Booking</h2>
+    <h3>{{ movie.title }}</h3>
+    <p>Available Seats: {{ availableSeats }}</p>
+    <form @submit.prevent="bookTicket">
+      <label for="seatNumber">Seat Number:</label>
+      <input type="number" id="seatNumber" v-model="seatNumber" min="1" max="64" required>
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name" required>
+      <label for="age">Age:</label>
+      <input type="number" id="age" v-model="age" min="1" required>
+      <button type="submit">Book</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'TicketBooking',
+  data() {
+    return {
+      movie: {},
+      availableSeats: 64,
+      seatNumber: null,
+      name: '',
+      age: null
+    };
+  },
+  created() {
+    this.fetchMovie();
+  },
+  methods: {
+    fetchMovie() {
+      const movieId = this.$route.params.id;
+      axios
+        .get(`https://seleksi-sea-2023.vercel.app/api/movies/${movieId}`)
+        .then((response) => {
+          this.movie = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    bookTicket() {
+      if (this.availableSeats > 0) {
+        this.availableSeats -= 1;
+      }
+        .post('https://seleksi-sea-2023.vercel.app/api/bookings', {
+          movieId: this.movie.id,
+          seatNumber: this.seatNumber,
+          name: this.name,
+          age: this.age,
+        })
+        .then((response) => {
+          // Handle successful booking
+          console.log('Ticket booked successfully!');
+        })
+        .catch((error) => {
+          // Handle booking error
+          console.log('Failed to book the ticket:', error);
+        });
+    },
+  },
+};
+</script>
+
         <input type="text" id="name" v-model="name" required>
         <label for="age">Age:</label>
         <input type="number" id="age" v-model="age" min="1" required>
@@ -33,8 +101,7 @@
     methods: {
       fetchMovie() {
         const movieId = this.$route.params.id
-        // Fetch movie data from API based on movieId
-        // Example using Axios library
+      
         axios.get(`https://seleksi-sea-2023.vercel.app/api/movies/${movieId}`)
           .then(response => {
             this.movie = response.data
